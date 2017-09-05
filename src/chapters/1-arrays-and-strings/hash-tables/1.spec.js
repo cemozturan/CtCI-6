0117eeb5d7b9dd2d90548942997fdcbc6ba4b9ef
Cem Ozturan <cozturan@findmypast.com>
2017-09-05 11:48:36 +0100

    Improving the tests for 1.1

--- src/chapters/1-arrays-and-strings/hash-tables/1.spec.js
+++ src/chapters/1-arrays-and-strings/hash-tables/1.spec.js
@@ -1,33 +1,43 @@
 import { isUniqueWithHashTable, isUniqueWithoutDataStructures } from './1';
 
 describe('Ch1-Q1: IsUnique', function() {
-  [
+  const uniqueCases = [
     '',
     ' ',
     'a',
     '123456789',
     '1q2w3e4r5t6y7u8i9ozasxdcfvgbhnjm[]#;'
-  ].forEach(testCase => {
-    it('returns true for unique strings', () => {
-      let resultWithHashTable = isUniqueWithHashTable(testCase);
-      let resultWithoutDataStructures = isUniqueWithoutDataStructures(testCase);
-      expect(resultWithHashTable).toBe(true);
-      expect(resultWithoutDataStructures).toBe(true);
-    })
-  });
+  ];
 
-  [
+  const nonUniqueCases = [
     '  ',
     'asdfghjka',
     'q2w3e4r5t6y75',
     'lkjlkj',
     'l#odfh#'
-  ].forEach(testCase => {
-    it('returns false for strings with duplicate characters', () => {
+  ];
+
+  uniqueCases.forEach(testCase => {
+    it(`returns true for unique strings using a hash table: ${testCase}`, () => {
       let resultWithHashTable = isUniqueWithHashTable(testCase);
+      expect(resultWithHashTable).toBe(true);
+    });
+
+    it(`returns true for unique strings without using a data structure: ${testCase}`, () => {
       let resultWithoutDataStructures = isUniqueWithoutDataStructures(testCase);
+      expect(resultWithoutDataStructures).toBe(true);
+    });
+  });
+
+  nonUniqueCases.forEach(testCase => {
+    it(`returns false for strings with duplicate characters using a hash table: ${testCase}`, () => {
+      let resultWithHashTable = isUniqueWithHashTable(testCase);
       expect(resultWithHashTable).toBe(false);
+    });
+
+    it(`returns false for strings with duplicate characters without using a data structure: ${testCase}`, () => {
+      let resultWithoutDataStructures = isUniqueWithoutDataStructures(testCase);
       expect(resultWithoutDataStructures).toBe(false);
-    })
+    });
   });
 })
